d0ba5741c4c9fa291b7cc23498242ef4f5231727
Thilip Dhanavel <87736175+thilipd@users.noreply.github.com>
2022-01-03 14:13:32 +0530

    Love-calculator

--- love-calculator/script.js
+++ love-calculator/script.js
@@ -6,6 +6,10 @@
 const name1 = document.querySelector('.n1');
 const name2 = document.querySelector('.n2');
 const calcBtn = document.querySelector('.calc');
+const display = document.querySelector('.display');
+const resultDisplay = document.querySelector('.result');
+const inputContainer = document.querySelector('.input_container');
+const resetBtn = document.querySelector('.reset');
 
 
 let name1Arr = [''];
@@ -19,75 +23,93 @@ let num = [];
 let nam = [];
 let c = 0;
 let alphaCount = [];
-
-let add = [];
 let c1 = 0;
 const luv = ['l', 'o', 'v', 'e'];
+
+let result;
 
 // eventlistner
 
 calcBtn.addEventListener('click', function () {
 
-    // if (name1.value === '' || name2.value === '') {
-    //     alert('Please enter the name')
-    // } else {}
+    if (name1.value === '' || name2.value === '') {
+        alert('Please enter the name')
+    } else {
 
-    // name1Arr = [name1.value];
-    // name2Arr = [name2.value];
-
-    name1Arr = ['Rajan'];
-    name2Arr = ['Rani'];
-
-    x1 = name1Arr[0].split("");
-    x2 = name2Arr[0].split("");
-
-    y = [...x1, ...luv, ...x2];
-
-    y.forEach((val, i) => y[i] = val.toLowerCase())
-
-    y.forEach(val => count[val] ? count[val] += 1 : count[val] = 1);
-
-    for (let props in count) {
-        num[c] = count[props];
-        nam[c] = props;
-        ++c;
-    }
+        name1Arr = [name1.value];
+        name2Arr = [name2.value];
 
 
 
-    alphaCount = [...num]
-    let len = Math.trunc(alphaCount.length / 2);
-    let z = alphaCount.length;
-    console.log(len, z)
+        x1 = name1Arr[0].split("");
+        x2 = name2Arr[0].split("");
 
+        y = [...x1, ...luv, ...x2];
 
+        y.forEach((val, i) => y[i] = val.toLowerCase())
 
-    for (let i = 0, j = z - 1; i < z - 1, j >= len; i++, j--) {
+        y.forEach(val => count[val] ? count[val] += 1 : count[val] = 1);
 
-        if (i === j) {
-            console.log(alphaCount[i] = alphaCount[i], i, j);
-            ++c1;
-        }
-        else if (i !== j) {
-            console.log(alphaCount[i] = alphaCount[i] + alphaCount[j], i, j);
-            ++c1;
+        for (let props in count) {
+            num[c] = count[props];
+            nam[c] = props;
+            ++c;
         }
 
-        if (alphaCount.length !== c1) {
-            for (let k = alphaCount.length - 1; k > c1; k--) {
-                console.log(k);
+        alphaCount = [...num]
+
+
+        while (alphaCount.length !== 2) {
+
+            for (let i = 0, j = alphaCount.length - 1; i < alphaCount.length - 1, j >= 0; i++, j--) {
+
+                if (i === j) {
+                    alphaCount[i] = alphaCount[i];
+                    ++c1;
+                }
+                else if (i < j) {
+                    alphaCount[i] = alphaCount[i] + alphaCount[j];
+                    ++c1;
+                } else if (i > j) {
+                    alphaCount.pop();
+                }
+
             }
+
         }
+        if (alphaCount[0] > 9 || alphaCount[1] > 9) {
+
+            result = alphaCount.reduce((a, b) => a + b);
+
+        } else result = Number(alphaCount.join(""));
+
+
+        resultDisplay.innerHTML = `${result}%`;
+        display.classList.remove("hidden");
+        inputContainer.classList.add("hidden");
+
     }
+});
 
+resetBtn.addEventListener('click', function () {
+    name1Arr = [''];
+    name2Arr = [''];
+    x1, x2, y;
+    count = {};
 
+    num = [];
+    nam = [];
+    c = 0;
+    alphaCount = [];
+    c1 = 0;
+    result;
+    name1.value = "";
+    name2.value = "";
 
+    display.classList.add("hidden");
+    inputContainer.classList.remove("hidden");
 
-
-    console.log(y, count, nam, num, alphaCount, len)
-})
-
-
+});
 
 
 
