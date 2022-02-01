667fc216ad07f3f7938b8a3835c6194a5a1ebb88
Thilip Dhanavel <87736175+thilipd@users.noreply.github.com>
2022-02-01 21:57:36 +0530

    update

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,74 +1,49 @@
 'use strict';
 
-let lat = 12, lng = 80;
 
-// const success = (position) => {
+const success = (position) => {
 
-//     lat = position.coords.latitude;
-//     lng = position.coords.longitude;
+    let lat = position.coords.latitude;
+    let lng = position.coords.longitude;
+    link(lat, lng);
 
-//     console.log(lat, lng)
-//     return position;
+}
 
-// }
+const err = (error) => {
+    console.error(error);
+};
 
-// const err = (error) => {
-//     console.error(error);
-// };
+const link = (lat, lng) => {
 
+    const weatherRequest = new XMLHttpRequest();
 
-// navigator.geolocation.getCurrentPosition(success, err)
+    weatherRequest.open('GET', `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=8dbd483a60dfe6f894a00bc08dba7e18`);
 
+    weatherRequest.send();
+    weatherRequest.onload = function () {
+        let data = JSON.parse(weatherRequest.response);
 
-// const weatherRequest = new XMLHttpRequest();
+        console.log(data)
 
-// weatherRequest.open('GET', `https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=${lat}.5&lon=${lng}`);
+        console.log(`
 
-// weatherRequest.setRequestHeader("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com");
-// weatherRequest.setRequestHeader("x-rapidapi-key", "de91c2b642mshbce59aac48eaec1p1d1cd3jsnb154cb09350f");
+                 sunrise: ${data.city.sunrise},
+                 sunset : ${data.city.sunset},
+         `)
 
-// weatherRequest.send();
-// weatherRequest.onload = function () {
-//     let data = JSON.parse(weatherRequest.response);
+        data.list.forEach(v => {
+            console.log(`
 
-//     console.log(data)
+                    timestamp: ${v.dt},
+                    humidity: ${v.main.humidity},
+                    weather:${v.weather[0].description},
+                    temperature:${v.main.temp}
 
-//     data.data.forEach(v => {
-//         console.log(`
-
-//             weather : ${v.weather.description},
-//             temprature: ${v.temp},
-//             windspeed : ${v.wind_spd}'
-//             time_local : ${v.timestamp_local}
-
-//         `)
-//     })
-// }
-
-// const sunTimeRequest = new XMLHttpRequest();
-
-// sunTimeRequest.open("GET", `https://sunrise-sunset-times.p.rapidapi.com/getSunriseAndSunset?date=2022-01-31&latitude=${lat}&longitude=${lng}&timeZoneId=America%2FNew_York`);
-// sunTimeRequest.setRequestHeader("x-rapidapi-host", "sunrise-sunset-times.p.rapidapi.com");
-// sunTimeRequest.setRequestHeader("x-rapidapi-key", "de91c2b642mshbce59aac48eaec1p1d1cd3jsnb154cb09350f");
-
-// sunTimeRequest.send();
-
-// sunTimeRequest.onload = function () {
-//     let suntimeData = JSON.parse(sunTimeRequest.response);
-
-//     let x = suntimeData.sunrise;
-//     console.log(x.indexOf('T'))
-//     console.log(x.slice(11, 16))
-//     console.log(`
-//               sunset : ${suntimeData.sunrise},
-//               sunraise  : ${suntimeData.sunset}
-
-//         `, suntimeData, x)
-// }
-
-let now = new Date();
-
-console.log(`${}`)
+             `)
+        })
+    }
+}
 
 
 
+navigator.geolocation.getCurrentPosition(success, err)
